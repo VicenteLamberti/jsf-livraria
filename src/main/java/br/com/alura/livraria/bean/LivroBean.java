bd042a1abe6153d986a935f062c0fdcf5cab494f
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2022-11-09 09:43:21 -0300

    Permitindo editar livro, mesmo com lazy

--- src/main/java/br/com/alura/livraria/bean/LivroBean.java
+++ src/main/java/br/com/alura/livraria/bean/LivroBean.java
@@ -134,7 +134,7 @@ public class LivroBean implements Serializable{
 
 	public void carregar(Livro livro) {
 		System.out.println("Carregando livro");
-		this.livro = livro;
+		this.livro = dao.buscaPorId(livro.getId());
 	}
 
 	@Transacional
